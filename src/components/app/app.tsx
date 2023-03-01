661da6d4896c587fd7f6fa389aad8baafe26dc7f
1Magz1 <maxim.chipurnoff@gmail.com>
2023-03-01 17:09:03 +0600

    init

---
+++ src/components/app/app.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import reactLogo from '../../assets/icons/react.svg';
+import './app.scss';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="card">
+        <button
+          type="button"
+          onClick={() => setCount(() => count + 1)}
+        >
+          count is
+          {' '}
+          {count}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
